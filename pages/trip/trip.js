179d2a9abe800a097e5ce3c91ed2513c875837dc
Raul Sauco <sauco.raul@gmail.com>
2018-12-06 19:51:13 +0800

    Implemented pages/trip

--- pages/trip/trip.js
+++ pages/trip/trip.js
@@ -8,23 +8,44 @@ Page({
    */
   data: {
     haveTrip: false,
-    trip: {}
+    trip: {},
+    images: []
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    // console.log(options);
+    
+    // Set the trip
     this.setData({
       trip: trips[options.id -1]
     });
 
+    // If the trip was setup correctly use the data
     if (this.data.trip !== {}) {
+
       console.debug('Displaying details for trip ' + this.data.trip.id);
       this.setData({haveTrip: true});
+
+      wx.setNavigationBarTitle({
+        title: this.data.trip.name,
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      });
+
+      this.generateFakeImages();
+
     } else {
+
       console.warn('Could not obtain trip ' + options.id);
+      wx.setNavigationBarTitle({
+        title: '错误',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
     }
   },
 
@@ -75,5 +96,25 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * Handle clicks on the trip thumbnail images
+   */
+  clickImage: function (e) {
+    console.log('Registered click on ' + e.currentTarget.dataset.imageid);
+  },
+
+  generateFakeImages: function () {
+    
+    var total = 100;
+    var url = 'https://placeimg.com/50/50/nature/';
+    var images = [];
+
+    for (var i=0 ; i < total ; i++) {      
+      images.push(url + i);
+    }
+
+    this.setData({images: images});
   }
 })
