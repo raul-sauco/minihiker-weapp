6b5cc2c92b507c84d7b15667852c649d31f96131
Raul Sauco <sauco.raul@gmail.com>
2019-01-03 13:44:58 +0800

    ProgramProvider fetches program-types

--- helpers/programProvider.js
+++ helpers/programProvider.js
@@ -56,9 +56,46 @@ class ProgramProvider {
       program.weapp_cover_image = this.resUrl + 'img/pg/' + 
         program.id + '/' + program.weapp_cover_image;
       
+      // Fetch the program type
+      if (this.programTypes[program.type_id] === undefined) {
+        console.log('unknown program type, go get it');
+        this.fetchProgramType(program.type_id);
+      } else {
+        console.log('known program type, already have it');
+      }
       
     });
   }
+
+/**
+ * Fetch a program-type from the server
+ */
+  fetchProgramType(id) {
+
+    console.log('Sending Request for program-type ' + id);
+
+    var endpoint = 'program-types/' + id;
+
+    wx.request({
+      url: this.url + endpoint,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: (res) => {
+        // If the request is successful we should get programs back
+        this.programTypes[id] = res.data;
+        console.log(this.programTypes);
+      },
+      fail: (res) => {
+        console.warn('Request failed. ' + this.url + endpoint);
+      },
+      complete: (res) => {
+        console.log('Request completed. ' + this.url + endpoint);
+      }
+    });
+
+  }
+
 }
 
 module.exports = ProgramProvider;
