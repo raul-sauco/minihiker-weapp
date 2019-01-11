df6c35c2f4ed21b301beea3a1f28407cb0514f38
Raul Sauco <sauco.raul@gmail.com>
2019-01-11 19:52:20 +0800

    Added PageNational
    There is a lot of repetition, will need to extract wxml and wxss to a template

---
+++ pages/national/national.js
@@ -0,0 +1,77 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    programGroups: [],
+    resUrl: null
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function () {
+
+    this.fetchProgramGroups();
+
+  },
+
+  /**
+   * Fetch program groups from the server
+   */
+  fetchProgramGroups: function () {
+
+    // TODO allow for pagination
+
+    wx.showLoading({
+      title: '获取数据',
+    });
+
+    // We are fetching international programs
+    var endpoint = 'program-groups?weapp_visible=true&int=false&expand=location,programs,type,programs.registrations.programs.period';
+
+    wx.request({
+      url: app.globalData.url + endpoint,
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': app.globalData.authToken
+      },
+      success: (res) => {
+        // If the request is successful we should get programGroups back   
+        this.setData({
+          resUrl: app.globalData.resUrl,
+          programGroups: res.data,
+          isRegistrationOpen: this.isRegistrationOpen
+        });
+
+        wx.hideLoading();
+      },
+      fail: (res) => {
+        console.warn('Request failed. ' + app.globalData.url + endpoint);
+      },
+      complete: (res) => {
+        console.log('Request completed. ' + app.globalData.url + endpoint);
+      }
+    });
+
+  },
+
+  /**
+   * Navigate to one ProgramGroup's page
+   */
+  showProgram: function (event) {
+
+    var targetProgramId = event.currentTarget.dataset.programid;
+
+    var pg = this.data.programGroups.find(item => {
+      return item.id === parseInt(targetProgramId, 10);
+    });
+
+    // Set the ProgramGroup on the provider
+    app.globalData.programProvider[pg.id] = pg;
+
+    wx.navigateTo({
+      url: '../program/program?id=' + targetProgramId,
+    });
+  },
+})
