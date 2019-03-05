33121c37139df6ed16636b212c48d6a01c8df839
Raul Sauco <sauco.raul@gmail.com>
2019-03-05 11:03:55 +0800

    Removed log calls on me.js

--- pages/me/me.js
+++ pages/me/me.js
@@ -20,8 +20,6 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-
-      console.log(this.data.userInfo)
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -39,9 +37,7 @@ Page({
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
-          })
-          console.log('yayayaya')
-          console.log(this.data.userInfo)
+          });
         }
       })
     }
