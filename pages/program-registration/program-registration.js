5f4bf6f5ef30bd66df4519aa1ed50b449832aa45
Raul Sauco <sauco.raul@gmail.com>
2019-05-12 18:32:25 +0800

    Formatted program-registration, wx.requestPayment logic placeholder

--- pages/program-registration/program-registration.js
+++ pages/program-registration/program-registration.js
@@ -29,6 +29,9 @@ Page({
       return program.id == options.p;
     });
 
+    p.formattedStartDate = this.formatDate(p.start_date);
+    p.formattedEndDate = this.formatDate(p.end_date);
+
     this.setData({
       programGroup: pg,
       program: p,
@@ -67,9 +70,89 @@ Page({
 
   },
 
+  /**
+   * Process client payment for a given program.
+   */
   processPayment: function () {
-    console.log('Selected clients: ' + this.data.selectedClients.toString());
-    console.log('Registering for program ' + this.data.program.id);
+
+    let selectedClients = this.data.selectedClients;
+    let programId = this.data.program.id;
+    let timestamp = Math.floor(Date.now() / 1000);
+    let nonceStr = this.generateRandomString(12);
+
+    wx.showLoading({
+      title: '处理付款',
+    });
+
+    // TODO change this for real wx.requestPayment call
+    setTimeout(() => {
+      wx.hideLoading();
+      wx.showToast({
+        title: '正在开发中',
+        icon: "success"
+      });
+      setTimeout(() => {
+        wx.navigateTo({
+          url: '/pages/program-group/program-group?id=' + this.data.programGroup.id,
+        });
+      }, 1500);
+    }, 2000);
+
+    /**
+    wx.requestPayment({
+      'timeStamp': timestamp,
+      'nonceStr': nonceStr,
+      'package': '',
+      'signType': 'MD5',
+      'paySign': '',
+      'success': function (res) {
+
+        console.log(res);
+
+      },
+      'fail': function (res) {
+
+        console.log(res);
+
+      }
+    });
+    */
+
+  },
+
+  /**
+   * Generate a random string of a required length
+   */
+  generateRandomString: function (length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for( var i = 0; i<length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  },
+
+  /**
+   * Return a formatted version of the date.
+   */
+  formatDate: function (dateString) {
+    const currentYear = new Date().getFullYear();
+
+    let dates = dateString.split("-");
+    let formattedDate = '';
+
+    if (currentYear !== parseInt(dates[0])) {
+
+      formattedDate += dates[0] + '年';
+
+    }
+
+    formattedDate += dates[1] + '月';
+    formattedDate += dates[2] + '日';
+
+    return formattedDate;
+
   },
 
   /**
