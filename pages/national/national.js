08ece97c7cb5328e94d1d27271663405e116e9b9
Raul Sauco <sauco.raul@gmail.com>
2019-05-23 20:22:26 +0800

    Added filters to page-national

--- pages/national/national.js
+++ pages/national/national.js
@@ -3,7 +3,113 @@ const app = getApp()
 
 Page({
   data: {
-    programGroups: []
+    programGroups: [],
+    filters: [
+      {
+        title: '单日活动',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '单日活动'
+        }
+      },
+      {
+        title: '周末',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '周末'
+        },
+      },
+      {
+        title: '寒假',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '寒假'
+        }        
+      },
+      {
+        title: '暑假',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '暑假'
+        },
+      },
+      {
+        title: '清明',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '清明'
+        }
+      },
+      {
+        title: '春假',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '春假'
+        }
+      },
+      {
+        title: '五一',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '五一'
+        }
+      },
+      {
+        title: '端午',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '端午'
+        }
+      },
+      {
+        title: '国庆',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '国庆'
+        }
+      },// start
+      {
+        title: '圣诞',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '圣诞'
+        },
+      },
+      {
+        title: '元旦',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '元旦'
+        }
+      },
+      {
+        title: '夏令营',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '夏令营'
+        }
+      },
+      {
+        title: '冬令营',
+        active: false,
+        query: {
+          parameter: 'type',
+          value: '冬令营'
+        }
+      }    
+    ]
   },
 
   /**
@@ -27,7 +133,9 @@ Page({
     });
 
     // We are fetching national programs
-    var endpoint = 'program-groups?weapp_visible=true&int=false&expand=location,programs,type,programs.registrations,programs.period';
+    let endpoint = 'program-groups?weapp_visible=true&int=false&expand=location,programs,type,programs.registrations,programs.period';
+
+    endpoint = this.addFiltersToEndpoint(endpoint);
 
     wx.request({
       url: app.globalData.url + endpoint,
@@ -54,6 +162,55 @@ Page({
         console.log('Request completed. ' + app.globalData.url + endpoint);
       }
     });
+
+  },
+
+  /**
+   * Change the active status of the clicked filter and refresh the programs based on the new 
+   * filter.
+   */
+  toggleFilter: function (e) {
+
+    let filter = e.currentTarget.dataset.filter;
+    let filters = this.data.filters;
+
+    filters.forEach(f => {
+
+      if (f.title == filter) {
+        // Toggle the current filter
+        f.active = !f.active;
+      } else {
+
+        // Deactivate all other filters
+        f.active = false;
+
+      }
+
+    });
+
+    this.setData({
+      filters: filters
+    });
+
+    this.fetchProgramGroups();
+
+  },
+
+  /**
+   * Add filters to the request endpoint
+   */
+  addFiltersToEndpoint: function (endpoint) {
+
+    // TODO allow for multiple filters
+
+    this.data.filters.forEach(f => {
+
+      if (f.active) {
+        endpoint += '&' + f.query.parameter + '=' + f.query.value; 
+      }
+    });
+
+    return endpoint;
 
   },
 
