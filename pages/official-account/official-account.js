01c35eee419fc28a51d17b4bc03f146baae9c2c5
Raul Sauco <sauco.raul@gmail.com>
2020-03-25 23:27:33 +0800

    Update library version to 2.10.3. Add official-account page template

--- pages/official-account/official-account.js
+++ pages/official-account/official-account.js
@@ -1,2 +1,20 @@
 Page({
+
+  data: {
+    hasError: false,
+    error: null
+  },
+
+  bindload: function (e) {
+    console.log('load');
+  },
+
+  binderror: function (e) {
+    this.setData({
+      hasError: true,
+      error: e.detail.errMsg
+    })
+    
+    console.log(e);
+  }
 })
