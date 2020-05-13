079f34e1a1782e5005cab4e77b17a7309f3d8da8
Raul Sauco <sauco.raul@gmail.com>
2020-05-13 16:32:05 +0200

    Added official account QR codes

--- pages/me/me.js
+++ pages/me/me.js
@@ -10,7 +10,9 @@ Page({
     hasUserInfo: false,
     userInfo: null,
     resUrl: app.globalData.resUrl,
+    staticUrl: app.globalData.staticUrl,
     accountInfo: null,
+    qrCodeVisible: false
   },
 
   /**
@@ -49,5 +51,25 @@ Page({
       });
       
     }
+  },
+
+  /**
+   * Bind to the official-account load event
+   */
+  officialAccountLoad: function (e) {
+    console.debug('Loaded official-account button', e.detail);
+    this.setData({
+      qrCodeVisible: false
+    });
+  },
+
+  /**
+   * Bind to the official-account error event
+   */
+  officialAccountError: function (e) {
+    console.log('Error loading official-account button', e);
+    this.setData({
+      qrCodeVisible: true
+    });
   }
 })
