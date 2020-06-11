805e2304017793467167b8b67202e9186b6e2713
Raul Sauco <sauco.raul@gmail.com>
2020-06-11 12:27:08 +0200

    Updated confirm payment message

--- pages/confirm-payment/confirm-payment.js
+++ pages/confirm-payment/confirm-payment.js
@@ -16,29 +16,55 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    wx.showNavigationBarLoading();
+    // Try fetching the programGroup and program from the programProvider
+    const programGroup = app.globalData.programProvider.get(options.pg);
+    let program;
+    if (programGroup) {
+      program = programGroup.programs.find(p => p.id === +options.p);
+    }
+    if (program) {
+      const price = program.prices.find(pr => pr.id === +options.price);
+      this.setProperties(programGroup, program, price);
+    } else {
+      app.globalData.programProvider.fetchProgramGroup(options.pg).then(
+        pg => {
+          const p = pg.programs.find(p => p.id === +options.p);
+          const pr = p.prices.find(pr => pr.id === +options.price);
+          this.setProperties(pg, p, pr);
+        }).catch(err => {
+        console.error(err);
+        wx.hideNavigationBarLoading();
+        wx.showToast({
+          title: '下载错误',
+          icon: 'none',
+          duration: 4000
+        });
+      });
+    }
+  },
 
-    // Fetch the programGroup and program from the global programProvider
-    const pg = app.globalData.programProvider.get(options.pg),
-      program = pg.programs.find( p => p.id === +options.p ),
-      price = program.prices.find( pr => pr.id === +options.price);
-
+  /**
+   * Set the page properties once we have the program info
+   */
+  setProperties: function(pg, program, price) {
+    wx.hideNavigationBarLoading();
     this.setData({
       accountInfo: app.globalData.accountInfoProvider,
       programGroup: pg,
       program: program,
       price: price
     });
-
   },
 
   /**
    * Navigate to the select participant page.
    */
-  selectParticipants: function () {
+  selectParticipants: function() {
 
     wx.navigateTo({
-      url: "/pages/select-participants/select-participants?pg=" + this.data.programGroup.id + 
+      url: "/pages/select-participants/select-participants?pg=" + this.data.programGroup.id +
         "&p=" + this.data.program.id + "&price=" + this.data.price.id
     });
 
